9dd104fa0d3d3af9f4dbec91e8fca6c54d477265
santosh7vasa <santosh7vasa@outlook.com>
2020-04-04 21:34:44 +0530

    Rest_framework added

--- blog/models.py
+++ blog/models.py
@@ -22,3 +22,6 @@ class Post(models.Model):
 class Language(models.Model):
     name = models.CharField(max_length=50)
     paradigm = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
