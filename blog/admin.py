ca31bc5a05ce49ea758b3fcbfdd0279d5e21b375
santosh7vasa <santosh7vasa@outlook.com>
2020-04-13 19:00:30 +0530

    mongodb changes
    Merging

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Post
-from .models import Language
+from .models import Language, Device
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Language)
+admin.site.register(Device)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from djongo import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -13,6 +13,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
     def __str__(self):
         return self.title
 
@@ -25,3 +26,14 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+class Reading(models.Model):
+    reading1 = models.IntegerField()
+
+class Device(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.BooleanField()
+    power_reading = models.ArrayField(model_container=int)
+
+    def __str__(self):
+        return self.title
